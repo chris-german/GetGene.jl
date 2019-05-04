54205e4c27eea9c1853d32dc3c604a1f55f125d4
Chris German <germc3@ucla.edu>
2019-05-04 02:20:24 -0700

    add more error catching

--- src/getgenes.jl
+++ src/getgenes.jl
@@ -3,7 +3,13 @@
 
 #takes "rs" out of snpids/rsids if they start with "rs###"
 function getSNPID(snpid)
-    return match(r"(?<=rs)\w+", snpid).match
+    id = ""
+    try
+        id = match(r"(?<=rs)\w+", snpid).match
+    catch
+        id = snpid   
+    end
+    return id 
 end
 
 #checks if gene information is present, if not, return no gene found
@@ -12,19 +18,33 @@ function genecheck(info)
     try 
         locus = info["primary_snapshot_data"]["allele_annotations"][1]["assembly_annotation"][1]["genes"][1]["locus"]
     catch
-        locus = "No gene found"
+        locus = "No gene listed"
     end
     return locus
+end
+
+function httpcheck(snpid)   
+    http = HTTP.Messages.Response()
+    try
+        http = HTTP.get("https://api.ncbi.nlm.nih.gov/variation/v0/beta/refsnp/" * snpid)
+    catch
+        http = HTTP.Messages.Response()
+    end
+    return http
 end
 
 function getgenes(snpids)
     snpids = getSNPID.(snpids)
     loci = Vector{String}(undef, 0)
     for snpid in snpids
-        http = HTTP.get("https://api.ncbi.nlm.nih.gov/variation/v0/beta/refsnp/" * snpid)
-        str = String(http.body)
-        info = LazyJSON.parse(str)
-        locus = genecheck(info)
+        http = httpcheck(snpid)
+        if isempty(http.body)
+            locus = "snpid not in database"
+        else 
+            str = String(http.body)
+            info = LazyJSON.parse(str)
+            locus = genecheck(info)
+        end
         push!(loci, locus)
     end
     return loci
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,11 @@
 using Test, GetGene
 
 snps = ["rs113980419", "rs17367504", 
-"rs113980419", "rs2392929", "rs11014171"]
+"rs113980419", "rs2392929", "rs11014171",
+"78067132", "1465537", "Affx-4150211"]
 loci = ["C1orf167", "MTHFR", "C1orf167", 
-"No gene found", "CACNB2"]
+"No gene listed", "CACNB2", "No gene listed",
+"No gene listed", "snpid not in database"]
 
 
 @testset "snpid match" begin
