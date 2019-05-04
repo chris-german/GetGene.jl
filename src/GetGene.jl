01f8c2199d07823a8888090e3bd1c6ca8bfe0356
Chris German <chrisagerman@gmail.com>
2019-05-03 17:09:26 -0700

    add initial files

---
+++ src/GetGene.jl
@@ -0,0 +1,9 @@
+module GetGene
+
+using HTTP, JSON
+
+export
+    getgenes
+include("getgenes.jl")
+
+end
---
+++ src/getgenes.jl
@@ -0,0 +1,2 @@
+#Code to extract genes from NCBI database using their API 
+#API found here: https://api.ncbi.nlm.nih.gov/variation/v0/
